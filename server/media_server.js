861e619778bad26b45d2c988fc85a95d7598348d
Waleed Ahmad <waleedgplus@gmail.com>
2019-04-07 18:53:47 +0500

    Upgraded node-media-server to 2.0.1

--- server/media_server.js
+++ server/media_server.js
@@ -1,25 +1,10 @@
-const {NodeMediaServer} = require('node-media-server'),
+const NodeMediaServer = require('node-media-server'),
     config = require('./config/default').node_media_server,
     User = require('./database/Schema').User;
 
 nms = new NodeMediaServer(config);
 
-nms.on('preConnect', (id, args) => {
-    console.log('[NodeEvent on preConnect]', `id=${id} args=${JSON.stringify(args)}`);
-    // let session = nms.getSession(id);
-    // session.reject();
-});
-
-/*nms.on('postConnect', (id, args) => {
-    console.log('[NodeEvent on postConnect]', `id=${id} args=${JSON.stringify(args)}`);
-});
-
-nms.on('doneConnect', (id, args) => {
-    console.log('[NodeEvent on doneConnect]', `id=${id} args=${JSON.stringify(args)}`);
-});*/
-
 nms.on('prePublish', async (id, StreamPath, args) => {
-
     let stream_key = getStreamKeyFromStreamPath(StreamPath);
     console.log('[NodeEvent on prePublish]', `id=${id} StreamPath=${getStreamKeyFromStreamPath(StreamPath)} args=${JSON.stringify(args)}`);
 
@@ -32,28 +17,6 @@ nms.on('prePublish', async (id, StreamPath, args) => {
         }
     });
 });
-
-/*nms.on('postPublish', (id, StreamPath, args) => {
-    console.log('[NodeEvent on postPublish]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
-});
-
-nms.on('donePublish', (id, StreamPath, args) => {
-    console.log('[NodeEvent on donePublish]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
-});
-
-nms.on('prePlay', (id, StreamPath, args) => {
-    console.log('[NodeEvent on prePlay]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
-    // let session = nms.getSession(id);
-    // session.reject();
-});
-
-nms.on('postPlay', (id, StreamPath, args) => {
-    console.log('[NodeEvent on postPlay]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
-});
-
-nms.on('donePlay', (id, StreamPath, args) => {
-    console.log('[NodeEvent on donePlay]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
-});*/
 
 const getStreamKeyFromStreamPath = (path) => {
     let parts = path.split('/');
