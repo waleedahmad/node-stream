5797124b0ac03fa6598624de8787612050c15a62
Waleed Ahmad <waleedgplus@gmail.com>
2019-04-07 20:19:15 +0500

    Removed toastr dependency

--- client/index.js
+++ client/index.js
@@ -4,19 +4,6 @@ import {BrowserRouter} from 'react-router-dom';
 import 'bootstrap';
 require('./index.scss');
 import Root from './components/Root.js';
-import toastr from 'toastr';
-
-toastr.options = {
-    "debug": false,
-    "positionClass": "toast-bottom-right",
-    "onclick": null,
-    "fadeIn": 300,
-    "fadeOut": 1000,
-    "timeOut": 5000,
-    "extendedTimeOut": 1000,
-    "newestOnTop": false
-};
-
 
 if(document.getElementById('root')){
     ReactDOM.render(
